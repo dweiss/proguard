146a1603fe827e9ac83d0ca9ea367840724cea9c
Dawid Weiss <dawid.weiss@carrotsearch.com>
2025-05-21 10:54:42 +0200

    Revert formatting.

--- base/src/main/java/proguard/obfuscate/PackageRenameRule.java
+++ base/src/main/java/proguard/obfuscate/PackageRenameRule.java
@@ -5,25 +5,30 @@ import java.util.regex.Pattern;
 /**
  * A very simplistic package rename rule.
  */
-public final class PackageRenameRule {
+public final class PackageRenameRule
+{
     private final Pattern pattern;
     private final String replacement;
 
-    public PackageRenameRule(Pattern pattern, String replacement) {
+    public PackageRenameRule(Pattern pattern, String replacement)
+    {
         this.pattern = pattern;
         this.replacement = replacement;
     }
 
     @Override
-    public String toString() {
+    public String toString()
+    {
         return pattern + "=>" + replacement;
     }
 
-    public boolean shouldRename(String name) {
+    public boolean shouldRename(String name)
+    {
         return pattern.matcher(name).find();
     }
 
-    public String getNewClassName(String name) {
+    public String getNewClassName(String name)
+    {
         return pattern.matcher(name).replaceAll(replacement);
     }
 }
